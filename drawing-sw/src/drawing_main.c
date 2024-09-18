0812658b345f885267ae797407aa46470c922f26
Daniel Flynn <flynn.da@northeastern.edu>
2024-09-18 15:55:50 -0400

    Slight add

--- drawing-sw/src/drawing_main.c
+++ drawing-sw/src/drawing_main.c
@@ -127,6 +127,7 @@ int main(void) {
     // Create a windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(1280, 960, "Drawing Canvas", NULL, NULL);
     if (!window) {
+        fprintf(stderr, "Failed to create GLFW window\n");
         glfwTerminate();
         return -1;
     }
